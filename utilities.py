91e53e35c1febd7ebbc8d45f189f5d18dabdf85f
Frederic-gci <frederic.fournier@gci.ulaval.ca>
2020-04-08 15:45:30 -0400

    Add 'clean' function to utilities.

--- utilities.py
+++ utilities.py
@@ -1,5 +1,12 @@
 import datetime
+import glob
+import os
 
 def now():
     time = datetime.datetime.now()
     return time.strftime("%Y-%m-%d %H:%M:%S - ")
+
+def clean(directory):
+    toRemoveList = glob.glob( directory + 'tmp*')
+    for fileToRemove in toRemoveList:
+        os.remove(fileToRemove)
