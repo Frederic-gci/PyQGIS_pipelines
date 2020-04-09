8e0716b0e75385691b0a69c13c583e39ab85a151
Frederic-gci <frederic.fournier@gci.ulaval.ca>
2020-04-09 10:46:26 -0400

    Add the last stage functions (finalMerge, finalSimplification, clean) in pipeline_runner.

--- pipeline_runner.py
+++ pipeline_runner.py
@@ -1,23 +1,27 @@
 import glob
 import concurrent.futures
 
-## Local scripts
+# Local scripts
 import infocrue as ic
 import utilities as utils
 
-## To avoid nasty deprecation warnings from the qgis code: 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
 def main():
-    fileList = glob.glob("D:/RawData/InfoCrue/0508_JacquesCartier_HECRAS_WSE/WSE*.tif")
-    print( utils.now() + "Starting execution")
-    outputDir = 'C:/DATA/INFO-Crue_tmp/'
+    fileList = glob.glob(
+        "D:/RawData/InfoCrue/0508_JacquesCartier_HECRAS_WSE/WSE*.tif")
+    outputDir = 'C:/DATA/INFO-Crue_tmp2/'
     e = concurrent.futures.ProcessPoolExecutor()
     for file in fileList:
         e.submit(ic.coverage, file, outputDir)
     e.shutdown(wait=True)
-    print( pipeline.now() + "Ending loop: e.shutdown returned")
+    ic.finalMerge(
+        fileTemplate=outputDir + 'C_poly*.shp',
+        outputFile=outputDir + 'JC_raw_coverage.shp')
+    ic.finalSimplification(
+        inputFile=outputDir + 'JC_raw_coverage.shp',
+        outputFile=outputDir + 'JC_simplified_coverage.shp'
+    )
+    utils.clean(outputDir)
+
 
 if __name__ == '__main__':
     main()
